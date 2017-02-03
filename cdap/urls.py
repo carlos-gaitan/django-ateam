2389bb56f4caaf0b09e7dd10c05842096cc9e49b
elcharly <elcharly@ningunemail.com>
2017-02-03 10:14:12 -0300

    Se crea el template y funcion para listar, queda funcionando.

--- cdap/urls.py
+++ cdap/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^persona_listar$', views.persona_list, name='persona_listar'),
 ]
--- cdap/views.py
+++ cdap/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Persona
 #from django.template import loader
 
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def persona_list(request):
+    persona = Persona.objects.all()
+    contexto = {'personas':persona}
+    return render(request, 'persona_list.html', contexto)
